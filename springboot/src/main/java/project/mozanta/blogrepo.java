5488a0de580503b695a659a5b10f39e2b6a9b7f5
mydemoid <94388547+mydemoid@users.noreply.github.com>
2022-08-15 14:40:32 +0530

    Delete springboot directory

--- springboot/src/main/java/project/mozanta/blogrepo.java
+++
@@ -1,9 +0,0 @@
-package project.mozanta;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface blogrepo extends MongoRepository<blog, String>{
-
-}
--- springboot/src/main/java/project/mozanta/userrepo.java
+++
@@ -1,9 +0,0 @@
-package project.mozanta;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface userrepo extends MongoRepository<user, String>{
-
-}
